1d37e8ba3b6febeece026cb36dfd56d29818717c
Abdur Rafay <a_rafay01@hotmail.com>
2025-03-16 15:21:15 +0500

    animation on all section

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,37 +1,63 @@
 import { assets, serviceData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Services = () => {
     return (
-        <div id='services' className='w-full px-[12%] py-10 scroll-mt-20'>
-            <h4 className=' text-center mb-2 text-lg font-Ovo'>What we Offer</h4>
-            <h2 className=' text-center text-5xl font-Ovo'>Our Services</h2>
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            id='services' className='w-full px-[12%] py-10 scroll-mt-20'>
 
-            <p className=' text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
-                We are here to maintain the financial information required by your 
+            <motion.h4
+                initial={{ y: -20, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.3 }}
+                className=' text-center mb-2 text-lg font-Ovo'>What we Offer
+            </motion.h4>
+
+            <motion.h2
+                initial={{ y: -20, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className=' text-center text-5xl font-Ovo'>Our Services</motion.h2>
+
+            <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.7 }}
+                className=' text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+                We are here to maintain the financial information required by your
                 investors, regulators, and management through our:
-            </p>
+            </motion.p>
 
-            <div className=' grid grid-cols-auto gap-6 my-10'>
-                {serviceData.map(({icon, title, description, link}, index) =>(
-                    <div key={index}
-                    className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black
+            <motion.div
+                initial={{opacity: 0 }}
+                whileInView={{opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.9 }}
+                className=' grid grid-cols-auto gap-6 my-10'>
+                {serviceData.map(({ icon, title, description, link }, index) => (
+                    <motion.div 
+                    whileHover={{scale: 1.05}}
+                    key={index}
+                        className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black
                      cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 
                      dark:hover:bg-darkHover dark:hover:shadow-white'
                     >
-                        <Image src={icon} alt='' className=' w-10'/>
+                        <Image src={icon} alt='' className=' w-10' />
                         <h3 className=' text-lg my-4 text-gray-700 dark:text-white'>{title}</h3>
                         <p className='text-sm text-gray-600 leading-5 dark:text-white/80'>{description}</p>
 
                         <a href={link} className='flex items-center gap-2 text-sm mt-5'>
-                            Read more <Image alt='' src={assets.right_arrow} className=' w-4'/>
+                            Read more <Image alt='' src={assets.right_arrow} className=' w-4' />
                         </a>
-                    </div>
+                    </motion.div>
                 ))}
-            </div>
+            </motion.div>
 
-        </div>
+        </motion.div>
     )
 }
 
